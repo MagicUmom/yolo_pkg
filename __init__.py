8eed70ff22f0986d405ab4f5211b5adfaff4e809
MagicPepe <ken51006b@gmail.com>
2021-06-28 13:48:36 +0800

    魔改了一波，不使用內建的flags套件了，直接寫成dict

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 from yolo_pkg.Train import VOC, YOLO_format
 
 import tensorflow as tf
-from absl import app, flags, logging
-from absl.flags import FLAGS
 from yolo_pkg.Darknet2tf.core.yolov4 import YOLO, decode, filter_boxes
 from yolo_pkg.Darknet2tf.core import utils
 from yolo_pkg.Darknet2tf.core.config import cfg
@@ -19,8 +17,8 @@ import glob
 
 class YOLO():
     def __init__(self):
-
-        # self.FLAGS = flags.FLAGS
+        
+        self.FLAGS = dict()
 
         self.IMAGES_DIR_PATH = None
         self.LABELS_DIR_PATH = None
@@ -245,62 +243,54 @@ class YOLO():
         # MODEL_TYPE = 'yolov4'
         # CLASSES_FILE = "./data/classes/coco.names"
 
-        flags.DEFINE_string('weights', WEIGHTS, 'path to weights file')
-        flags.DEFINE_string('output', OUTPUT , 'path to output')
-        flags.DEFINE_boolean('tiny', False, 'is yolo-tiny or not')
-        flags.DEFINE_integer('input_size', INPUT_SIZE, 'define input size of export model')
-        flags.DEFINE_float('score_thres', 0.2, 'define score threshold')
-        flags.DEFINE_string('framework', 'tf', 'define what framework do you want to convert (tf, trt, tflite)')
-        flags.DEFINE_string('model', MODEL_TYPE, 'yolov3 or yolov4')
-        flags.DEFINE_string('classes', CLASSES_FILE , 'classes defined path. eg: coco.names')
-
-        # FLAGS.weights =  WEIGHTS            # path to weights file
-        # FLAGS.output = OUTPUT               # path to output
-        # FLAGS.tiny = False                  # is yolo-tiny or not
-        # FLAGS.input_size = INPUT_SIZE       # define input size of export model
-        # FLAGS.framework = "tf"              # define what framework do you want to convert (tf, trt, tflite)
-        # FLAGS.model = MODEL_TYPE            # yolov3 or yolov4
-        # FLAGS.classes = CLASSES_FILE        # classes defined path. eg: coco.names
+        self.FLAGS["weights"] =  WEIGHTS            # path to weights file
+        self.FLAGS["output"] = OUTPUT               # path to output
+        self.FLAGS["tiny"] = False                  # is yolo-tiny or not
+        self.FLAGS["input_size"] = INPUT_SIZE       # define input size of export model
+        self.FLAGS["framework"] = "tf"              # define what framework do you want to convert (tf, trt, tflite)
+        self.FLAGS["model"] = MODEL_TYPE            # yolov3 or yolov4
+        self.FLAGS["classes"] = CLASSES_FILE        # classes defined path. eg: coco.names
+        self.FLAGS["score_thres"] = 0.2             # define score threshold
 
     def save_tf(self, WEIGHTS, OUTPUT, INPUT_SIZE=416, MODEL_TYPE="yolov4", CLASSES_FILE = ''):
         if CLASSES_FILE == '': 
             CLASSES_FILE = os.path.join(self.BASE_PATH,"Darknet2tf/data/classes/coco.names")
         self.set_conf(WEIGHTS,OUTPUT,INPUT_SIZE,MODEL_TYPE,CLASSES_FILE)
 
-        STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+        STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(self.FLAGS)
 
-        input_layer = tf.keras.layers.Input([FLAGS.input_size, FLAGS.input_size, 3])
-        feature_maps = YOLO(input_layer, NUM_CLASS, FLAGS.model, FLAGS.tiny)
+        input_layer = tf.keras.layers.Input([self.FLAGS["input_size"], self.FLAGS["input_size"], 3])
+        feature_maps = YOLO(input_layer, NUM_CLASS, self.FLAGS["model"], self.FLAGS["tiny"])
         bbox_tensors = []
         prob_tensors = []
-        if FLAGS.tiny:
+        if self.FLAGS["tiny"]:
             for i, fm in enumerate(feature_maps):
                 if i == 0:
-                    output_tensors = decode(fm, FLAGS.input_size // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
+                    output_tensors = decode(fm, self.FLAGS["input_size"] // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, self.FLAGS["framework"])
                 else:
-                    output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
+                    output_tensors = decode(fm, self.FLAGS["input_size"] // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, self.FLAGS["framework"])
                 bbox_tensors.append(output_tensors[0])
                 prob_tensors.append(output_tensors[1])
         else:
             for i, fm in enumerate(feature_maps):
                 if i == 0:
-                    output_tensors = decode(fm, FLAGS.input_size // 8, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
+                    output_tensors = decode(fm, self.FLAGS["input_size"] // 8, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, self.FLAGS["framework"])
                 elif i == 1:
-                    output_tensors = decode(fm, FLAGS.input_size // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
+                    output_tensors = decode(fm, self.FLAGS["input_size"] // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, self.FLAGS["framework"])
                 else:
-                    output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
+                    output_tensors = decode(fm, self.FLAGS["input_size"] // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, self.FLAGS["framework"])
             bbox_tensors.append(output_tensors[0])
             prob_tensors.append(output_tensors[1])
         pred_bbox = tf.concat(bbox_tensors, axis=1)
         pred_prob = tf.concat(prob_tensors, axis=1)
-        if FLAGS.framework == 'tflite':
+        if self.FLAGS["framework"] == 'tflite':
             pred = (pred_bbox, pred_prob)
         else:
-            boxes, pred_conf = filter_boxes(pred_bbox, pred_prob, score_threshold=FLAGS.score_thres, input_shape=tf.constant([FLAGS.input_size, FLAGS.input_size]))
+            boxes, pred_conf = filter_boxes(pred_bbox, pred_prob, score_threshold=self.FLAGS["score_thres"], input_shape=tf.constant([self.FLAGS["input_size"], self.FLAGS["input_size"]]))
             pred = tf.concat([boxes, pred_conf], axis=-1)
         model = tf.keras.Model(input_layer, pred)
-        utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
+        utils.load_weights(model, self.FLAGS["weights"], self.FLAGS["model"], self.FLAGS["tiny"])
         model.summary()
-        model.save(FLAGS.output)
+        model.save(self.FLAGS["output"])
 
     # ------- FOR Darknet2TF END ---------#
